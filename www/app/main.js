c74fb1b3f0999594dc4a311fbb937de7192eee4d
Robert Myers <robertmyers@Roberts-MacBook-Pro.local>
2015-05-15 12:29:44 -0500

    et tu brutus?

--- www/app/main.js
+++ www/app/main.js
@@ -9,6 +9,6 @@ require.config({
 define('main', ['game'], function (Game) {
     'use strict';
 
-    var game = new Game({ el: '#lygneo' });
+    var game = new Game({ el: '#ultimate' });
     game.init();
 });
